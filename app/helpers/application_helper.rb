b2f591b868ec43b16cfaa700eff6c752c7e958ed
AndyObtiva <andy.am@gmail.com>
2017-02-07 10:39:19 +0400

    Finished implementation of flash_alert_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,21 @@
 module ApplicationHelper
+  FLASH_VARS = {
+    notice: {
+      bootstrap_class: 'success', #green
+    },
+    alert: {
+      bootstrap_class: 'info', #blue
+    },
+    # alert: 'warning', #yellow
+    error: {
+      bootstrap_class: 'danger', #red
+    }
+  }
 
   def flash_alert_bar
-    flash_type_bootstrap_alert = {
-      notice: 'success', #green
-      alert: 'info', #blue
-      # alert: 'warning', #yellow
-      error: 'danger', #red
-    }
     raw(%w(notice alert error).map do |flash_type|
       <<-MULTI_LINE
-      <p class="#{flash_type} alert-dismissible fade alert alert-#{flash_type_bootstrap_alert[flash_type.to_sym]}" style="#{'display: none' if flash[flash_type].blank?}">
+      <p class="flash-#{flash_type} alert-dismissible fade alert alert-#{FLASH_VARS[flash_type.to_sym][:bootstrap_class]}" style="#{'display: none;' if flash[flash_type].blank?}">
         #{flash[flash_type]}
       </p>
       MULTI_LINE
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,34 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  context 'single flash var' do
-    describe 'flash_alert_bar' do
-      {
-        notice: 'success',
-        alert: 'info',
-        error: 'danger',
-      }.each do |flash_var, flash_css_class|
-        it "displays #{flash_var} flash var" do
-          message = 'Notice Appears'
-          flash[flash_var] = message
-          expect(flash_alert_bar).to include(message)
-        end
-        it "uses #{flash_css_class} css class for #{flash_var} flash var" do
-          flash[flash_var] = 'message'
-          expect(flash_alert_bar).to include(flash_css_class)
+  describe 'flash_alert_bar' do
+    context 'single flash var' do
+      ApplicationHelper::FLASH_VARS.each_pair do |flash_var, flash_props|
+        context flash_var do
+          let!(:message) {"#{flash_var} appears".titleize}
+          before {flash[flash_var] = message}
+          let!(:doc) {Nokogiri::HTML(flash_alert_bar)} #relies on before block
+          let!(:flash_var_alert_element) {doc.at_css(".flash-#{flash_var}")}
+          let!(:other_flash_vars) {ApplicationHelper::FLASH_VARS.keys - [flash_var]}
+          it "displays #{flash_var} flash var element" do
+            flash_var_alert_message = flash_var_alert_element.children.select(&:text?).map(&:to_s).join rescue ''
+            expect(flash_var_alert_message).to include(message)
+          end
+          it "displays #{flash_var} flash var message" do
+            flash_var_alert_style = flash_var_alert_element.attribute('style').value rescue ''
+            expect(flash_var_alert_style).to_not include('display: none;')
+          end
+          it "includes #{flash_var} bootstrap class" do
+            flash_var_alert_class = flash_var_alert_element.attribute('class').value rescue ''
+            expect(flash_var_alert_class).to include(flash_props[:bootstrap_class])
+          end
+          it "ensures other flash vars are hidden" do
+            other_flash_vars.each do |other_flash_var|
+              flash_var_alert_element = doc.at_css(".flash-#{other_flash_var}")
+              flash_var_alert_style = flash_var_alert_element.attribute('style').value rescue ''
+              expect(flash_var_alert_style).to include('display: none;')
+            end
+          end
         end
       end
     end
-  end
-  context 'multiple flash vars' do
-    xit "displays multiple flash vars" do
-      message = 'Notice Appears'
-      flash[:notice] = 'Notice Appears'
-      flash[:alert] = 'Alert Appears'
-      flash[:error] = 'Error Appears'
-      expect(flash_alert_bar).to include(message)
-      expect(flash_alert_bar).to include(message)
-      expect(flash_alert_bar).to include(message)
+    context 'multiple flash vars' do
+      it "displays two flash vars" do
+        flash[:notice] = 'Notice Appears'
+        flash[:alert] = 'Alert Appears'
+
+        doc = Nokogiri::HTML(flash_alert_bar)
+        notice_bar = doc.at_css(".flash-notice")
+        alert_bar = doc.at_css(".flash-alert")
+        error_bar = doc.at_css(".flash-error")
+
+        expect(notice_bar.attribute('style').value).to_not include('display: none;')
+        expect(alert_bar.attribute('style').value).to_not include('display: none;')
+        expect(error_bar.attribute('style').value).to include('display: none;')
+      end
+      it "displays all three flash vars" do
+        flash[:notice] = 'Notice Appears'
+        flash[:alert] = 'Alert Appears'
+        flash[:error] = 'Error Appears'
+
+        doc = Nokogiri::HTML(flash_alert_bar)
+        notice_bar = doc.at_css(".flash-notice")
+        alert_bar = doc.at_css(".flash-alert")
+        error_bar = doc.at_css(".flash-error")
+
+        expect(notice_bar.attribute('style').value).to_not include('display: none;')
+        expect(alert_bar.attribute('style').value).to_not include('display: none;')
+        expect(error_bar.attribute('style').value).to_not include('display: none;')
+      end
+    end
+    context 'no flash vars' do
+      it "displays no flash vars" do
+        ApplicationHelper::FLASH_VARS.each_pair do |flash_var, _|
+          doc = Nokogiri::HTML(flash_alert_bar)
+          notice_bar = doc.at_css(".flash-notice")
+          alert_bar = doc.at_css(".flash-alert")
+          error_bar = doc.at_css(".flash-error")
+
+          expect(notice_bar.attribute('style').value).to include('display: none;')
+          expect(alert_bar.attribute('style').value).to include('display: none;')
+          expect(error_bar.attribute('style').value).to include('display: none;')
+        end
+      end
     end
   end
 end
